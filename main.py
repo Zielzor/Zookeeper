e902f8402de4d716ac0cbb32566f114d7924c4e5
Lukasz <zielzor@gmail.com>
2020-06-08 13:30:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 camel = r"""
 Switching on camera from habitat with camels...
  ___.-''''-.
@@ -19,11 +17,87 @@ Switching on camera from habitat with camels...
             ; ;    ! !    ! !     ; ;
            ;,,      !,!   !,!     ;,;
            /_I      L_I   L_I     /_I
-Yey, our little camel is sunbathing!
-"""
+Yey, our little camel is sunbathing!"""
+
+lion = r"""
+Switching on camera from habitat with lions...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is croaking!"""
+
+deer = r"""
+Switching on camera from habitat with deers...
+   /|       |\
+`__\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+goose = r"""
+Switching on camera from habitat with lovely goose...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \
+ <_  `     (  <`<            \              `-._\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+This bird stares intently at you... (Maybe it's time to change the channel?)"""
+
+bat = r"""
+Switching on camera from habitat with bats...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\  W  //           <
+       /             /~---~\             \
+      /_            |       |            _\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\
+                ~-. /  \_/  \ .-~
+                   V         V
+It looks like this bat is fine."""
 
 rabbit = r"""
-Switching on camera from the habitat with rabbits...
+Switching on camera from habitat with rabbits...
          ,
         /|      __
        / |   ,-~ /
@@ -39,30 +113,21 @@ Switching on camera from the habitat with rabbits...
  /| ;-"~ _  l
 / l/ ,-"~    \
 \//\/      .- \
- Y        /    Y
+ Y        /    Y 
  l       I     !
  ]\      _\    /"\
 (" ~----( ~   Y.  )
-It seems there will be more rabbits soon!
----
-The end of the program. To check another habitat restart the watcher please.
-"""
+It seems there will be more rabbits soon!"""
 
-bat = r"""
-Switching on camera from the habitat with bats...
-_________________               _________________
- ~-.              \  |\___/|  /              .-~
-     ~-.           \ / o o \ /           .-~
-        >           \\  W  //           <
-       /             /~---~\             \
-      /_            |       |            _\
-         ~-.        |       |        .-~
-            ;        \     /        i
-           /___      /\   /\      ___\
-                ~-. /  \_/  \ .-~
-                   V         V
-It looks like this bat is fine.
----
-The end of the program. To check another habitat restart the watcher please.
-"""
 
+animals = [camel, lion, deer, goose, bat, rabbit]
+
+while True:
+  habitat = input("Which habitat # do you need?")
+  if habitat == "exit":
+    print("See you!")
+    break
+  elif int(habitat) > len(animals):
+    print("No such habitat, try again")
+  else:
+    print(animals[int(habitat)])
